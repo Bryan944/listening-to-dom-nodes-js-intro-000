8dddbe15e6c936fb661a4e053d94e8a2a4f52334
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-05-16 06:56:42 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,3 +5,28 @@ const main = document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e){
+console.log(e.key);
+});
+
+const input2 = document.querySelector('input');
+
+input2.addEventListener('keydown', function(h){
+  if(h.key === g){
+    return h.preventDefault();
+  }else{
+    console.log(h.key);
+  }
+})
+
+let divs = document.querySelectorAll('div');
+
+function bubble(e){
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+for(let i = 0; i < divs.length; i++){
+  divs[i].addEventListener('click', bubble);
+}
